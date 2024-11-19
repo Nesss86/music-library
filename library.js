49b6cbd6edebe41dd8133f6b5cbdfacd89ffbf55
Nesss86 <Nessymonster86@gmail.com>
2024-11-18 22:28:30 -0500

    Completed second function

--- library.js
+++ library.js
@@ -44,8 +44,13 @@ const printPlaylists = function() {
 // t02: Model View Controller by James Dempsey (WWDC 2003)
 // t03: Four Thirty-Three by John Cage (Woodstock 1952)
 const printTracks = function() {
-
-}
+       for (let trackId in library.tracks) {
+         const track = library.tracks[trackId];
+         console.log(`${track.id}: ${track.name} by ${track.artist} (${track.album})`);
+       }
+     };
+     
+printTracks();
 
 
 // prints a list of tracks for a given playlist, using the following format:
